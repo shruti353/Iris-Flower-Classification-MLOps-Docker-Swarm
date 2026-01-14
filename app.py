5ece2e22ccf6e54cd40f2c534dade581196d29bb
shruti353 <shrutithakkar4667@gmail.com>
2026-01-14 11:32:55 +0530

    app.py

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 from flask import Flask, request, jsonify
+import numpy as np
 import joblib
 
 app = Flask(__name__)
-
-# Load trained model
 model = joblib.load("model.pkl")
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-    features = data["features"]
-
-    prediction = model.predict([features])[0]
-
-    return jsonify({
-        "prediction": int(prediction)
-    })
+    features = np.array(data["features"]).reshape(1, -1)
+    prediction = model.predict(features)[0]
+    return jsonify({"prediction": prediction})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
